23d216d8e93f65e7d5c531038be9bca1199ad725
Lee Youngmin <youngrx07@gmail.com>
2025-05-03 01:06:09 +0900

    Add dataset load function. Currently only available with MNIST dataset.

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
+### Import Torch Libraries
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 
+### Import Other Libraries
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from omegaconf import OmegaConf, DictConfig
 
-import myutils as my # Assuming myutils.py is in the same directory
+### Import Custom Libraries
+import myutils as my
 
 
 def main():
     print("hello, main")
 
+    ### Load the dataset
+    config:DictConfig = OmegaConf.load("config.yaml")
+    train_dataset, test_dataset = my.load_dataset(config.dataset)
+    print(f"Train dataset size: {len(train_dataset)}")
+
+
 
 if __name__ == "__main__":
-    my.check_cuda()
-    my.pre_settings()
-    print(pd.DataFrame(my.test_cuda(), columns=['a', 'b', 'c']))
+    # my.check_cuda()
     main()
--- myutils.py
+++ myutils.py
@@ -1,4 +1,9 @@
+### Import Torch Libraries
 import torch
+from torchvision import datasets, transforms
+
+from omegaconf import OmegaConf, DictConfig
+
 
 def check_cuda():
     if torch.cuda.is_available():
@@ -10,12 +15,32 @@ def check_cuda():
         print("CUDA is not available")
 
 
-def test_cuda():
+def test_cuda(print_it:bool=True):
     x = torch.rand(5, 3)
-    print(x)
+    if print_it:
+        print(x)
     return x
 
 
-def pre_settings():
-    print("### Pre Settings ###")
-    print("####### DONE #######")
+def get_yaml_settings(yaml:str ="config.yaml"):
+    config:DictConfig = OmegaConf.load(yaml)
+    return config
+
+
+def load_dataset(dataset:DictConfig):
+    if dataset == None:
+        raise ValueError("config file cannot be None")
+
+    train_dataset = None
+    test_dataset = None
+
+    if (dataset.name is None) or (dataset.dir is None):
+        raise ValueError("Dataset name or dir cannot be None")
+    elif dataset.name.lower() == "mnist":
+        transform = transforms.ToTensor()
+        train_dataset = datasets.MNIST(dataset.dir, train=True, download=True, transform=transform)
+        test_dataset = datasets.MNIST(dataset.dir, train=False, download=True, transform=transform)
+    else:
+        raise ValueError(f"Dataset name \'{dataset.name}\' is not supported")
+
+    return train_dataset, test_dataset
